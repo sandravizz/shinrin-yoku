4c52eafa2ac336ba0b03f9f92ca32376e21e0fd9
Sandra Becker <sandra.becker1982@gmail.com>
2024-09-10 15:35:36 +0100

    Improves events list

--- src/routes/events/+page.server.js
+++ src/routes/events/+page.server.js
@@ -1,13 +1,18 @@
-import { error as err } from '@sveltejs/kit';
+import { error as err } from "@sveltejs/kit";
 
 export const load = async () => {
-    try {
-        const result = await fetch(`https://api.meetup.com/lx-urban-gardening/events`)
-        const data = await result.json();
-        return {events: data};
-    }
-    catch (error) {
-        console.log(error);
-        throw err(400, `There was error.`);
-    }
-}
+  try {
+    const groupResult = await fetch(
+      `https://api.meetup.com/lx-urban-gardening`
+    );
+    const groupData = await groupResult.json();
+    const eventsResult = await fetch(
+      `https://api.meetup.com/lx-urban-gardening/events`
+    );
+    const eventsData = await eventsResult.json();
+    return { events: eventsData, group: groupData };
+  } catch (error) {
+    console.log(error);
+    throw err(400, `There was error.`);
+  }
+};
